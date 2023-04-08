5bece6c634a806919784272e994a9f11eb8157d7
Simatwa <smartwacaleb@gmail.com>
2023-04-09 00:33:31 +0300

    Append ads script

--- smartBetika/betika.py
+++ smartBetika/betika.py
@@ -329,6 +329,7 @@ def html_style(title: str):
     td{{border-left:1px solid rgb(42, 82, 65);border-right:1px solid green;}}
     tr{{border:none;}}</style>
     <h1>SmartBetika Predictions.</h1>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9839772411808918" crossorigin="anonymous"></script>
     <a href="#"><img style="display:none" src="https://visitor-badge.glitch.me/badge?page_id=Simatwa.smartBetika&left_color=red&right_color=lime&left_text=Punters" alt="Punters"/></a>
     <h4 class="hdr">{total}.{title}</h4>
     <marquee id="time">Dated : {date()}</marquee>"""
